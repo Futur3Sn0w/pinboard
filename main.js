b840c9c1b64c0446633968fb8c1c4dfdadd1083d
Adam (Futur3Sn0w) <futur3sn0w@gmail.com>
2022-07-23 12:53:54 -0400

    (v0.10.1)
    hotfix-test-1

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var root = document.querySelector(':root');
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
-            .register("/serviceWorker.js")
+            .register("serviceWorker.js")
             .then(res => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
     })
